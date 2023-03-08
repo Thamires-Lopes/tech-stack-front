6fdf4cff140d04c6b1cc5c45ff0252b09775a59f
Thamires Lopes <thamiresl686@gmail.com>
2023-03-07 22:01:41 -0300

    Adding some consts to TipoRelevancia enum

--- src/app/models/enums/tipo-relevancia.enum.ts
+++ src/app/models/enums/tipo-relevancia.enum.ts
@@ -2,4 +2,25 @@ export enum TipoRelevancia {
     Baixa = 1,
     Normal,
     Alta
-}
+};
+
+export const TiposRelevanciaList : {key: number, description: string}[]  = [
+    {
+        key: TipoRelevancia.Baixa,
+        description: "Baixa"
+    },
+    {
+        key: TipoRelevancia.Normal,
+        description: "Normal"
+    },
+    {
+        key: TipoRelevancia.Alta,
+        description: "Alta"
+    }
+];
+
+export const TiposRelevanciaNamesList : string[] = [
+    "Baixa",
+    "Normal",
+    "Alta"
+]
